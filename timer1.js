07d91fa5d159f7705c1740fbf1db6d7f72599528
derekb123 <derekbutvin@gmail.com>
2020-07-28 21:41:19 +0000

    updated for edge conditions

--- timer1.js
+++ timer1.js
@@ -3,9 +3,12 @@ let input = process.argv.slice(2);
 const beepers = (times) => {
   //let secs = 0;
   for (let time of times) {
-    setTimeout(() => {
-      console.log('beep');
-    }, (time * 1000));
+    timeN = Number(time);
+    if (timeN > 0 && typeof timeN === "number"){
+      setTimeout(() => {
+        console.log('beep');
+      }, (timeN * 1000));
+    }
   }
 }
 
